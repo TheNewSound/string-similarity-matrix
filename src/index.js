c0f2bcbd26d733e1f5ec70279382f892a54cd347
TheNewSound <TheNewSound@users.noreply.github.com>
2019-01-18 02:43:45 +0100

    skeleton code structure + add TensorFlowJS dependency

---
+++ src/index.js
@@ -0,0 +1,37 @@
+const compareStrings = (strings) => {
+    return new Promise((resolve, reject) => {
+        if (areArgsValid(strings)) {
+            if (detectWebGLContext() === true) {
+                /* if (typeof OffscreenCanvas !== 'undefined') { */
+                //webgl available
+                //run TFJS web worker
+                resolve("return TFJS data");
+            } else {
+                //run CPU web workers thread pool
+                resolve("return CPU data");
+            }
+        } else {
+            reject("Invalid arguments. Give an array of strings. Example: compareStrings([\"ELOQUENT\",\"ELOQUENTLY\"]).then(response => { console.log(response); });");
+        }
+    });
+};
+
+function areArgsValid(strings) {
+    if (!Array.isArray(strings)) return false;
+    if (!strings.length) return false;
+    if (strings.find(s => typeof s !== 'string')) return false;
+    return true;
+}
+
+function detectWebGLContext() {
+    const canvas = document.createElement("canvas");
+    const gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+
+    if (gl && gl instanceof WebGLRenderingContext) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+export default compareStrings;
